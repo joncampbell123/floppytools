faf1d95190eaec8defaff1ace6d1d170d364395a
Jonathan Campbell <jonathan@castus.tv>
2018-10-10 23:45:05 -0700

    read

--- kryoflux1.cpp
+++ kryoflux1.cpp
@@ -8,12 +8,131 @@
 #include <map>
 #include <vector>
 
+enum {
+    MSG_NONE,
+    MSG_FLUX,
+    MSG_OOB
+};
+
+struct kryoflux_event {
+    unsigned int                flux_interval;
+    unsigned char               message;
+    unsigned char               oob_code;
+    unsigned long               offset;
+    std::vector<unsigned char>  msg;
+};
+
+bool kryoflux_read(struct kryoflux_event &ev,FILE *fp) {
+    int c;
+
+    ev.msg.clear();
+    ev.oob_code = 0;
+    ev.message = MSG_FLUX;
+    ev.flux_interval = 0;
+    ev.offset = ftell(fp);
+
+    do {
+        if ((c = fgetc(fp)) < 0)
+            return false;
+
+        if (c <= 0x07) {
+            ev.flux_interval += ((unsigned char)c << 8u);
+
+            if ((c = fgetc(fp)) < 0)
+                return false;
+            ev.flux_interval +=  (unsigned char)c;
+
+            return true;
+        }
+        else if (c == 0x08) {
+            // ignore
+        }
+        else if (c == 0x09) {
+            if ((c = fgetc(fp)) < 0)
+                return false;
+
+            // ignore
+        }
+        else if (c == 0x0A) {
+            if ((c = fgetc(fp)) < 0)
+                return false;
+            if ((c = fgetc(fp)) < 0)
+                return false;
+
+            // ignore
+        }
+        else if (c == 0x0B) {
+            // overflow16
+            ev.flux_interval += 0x10000u;
+        }
+        else if (c == 0x0C) {
+            // value16
+            if ((c = fgetc(fp)) < 0)
+                return false;
+            ev.flux_interval +=  (unsigned char)c;
+
+            if ((c = fgetc(fp)) < 0)
+                return false;
+            ev.flux_interval += ((unsigned char)c << 8u);
+
+            return true;
+        }
+        else if (c == 0x0D) {
+            unsigned long oob_len;
+
+            // OOB
+            if ((c = fgetc(fp)) < 0)
+                return false;
+            unsigned char type = (unsigned char)c;
+
+            if ((c = fgetc(fp)) < 0)
+                return false;
+            oob_len  = (unsigned char)c;
+
+            if ((c = fgetc(fp)) < 0)
+                return false;
+            oob_len += (unsigned char)c << 8u;
+
+            ev.msg.clear();
+            ev.msg.resize(oob_len);
+            for (unsigned long i=0;i < oob_len;i++) {
+                if ((c = fgetc(fp)) < 0)
+                    return false;
+
+                ev.msg[i] = (unsigned char)c;
+            }
+
+            ev.oob_code = type;
+
+            return true;
+        }
+        else {
+            ev.flux_interval +=  (unsigned char)c;
+
+            return true;
+        }
+    } while (1);
+
+    return false;
+}
+
 int main(int argc,char **argv) {
     if (argc < 2) {
         fprintf(stderr,"%s <raw>\n",argv[0]);
         return 1;
     }
 
+    FILE *fp = fopen(argv[1],"rb");
+    if (fp == NULL) return 1;
+
+    struct kryoflux_event ev;
+
+    while (kryoflux_read(ev,fp)) {
+        fprintf(stderr,"flux=%lu(0x%lx) oob=%u ooblen=%zu offset=%lu\n",
+            ev.flux_interval,ev.flux_interval,ev.oob_code,ev.msg.size(),ev.offset);
+    }
+
+    fclose(fp);
     return 0;
 }
 
